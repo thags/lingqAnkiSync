d1b1e98ff3915604533a479094e47582c908f632
Tyler Hagstrom <tyler.hagstrom@gmail.com>
2023-03-18 18:24:20 -0500

    removing some duplication in the tests

--- Tests/test_Config.py
+++ Tests/test_Config.py
@@ -3,11 +3,12 @@ sys.path.append(
     os.path.realpath(f"{os.path.dirname(__file__)}/../LingqAnkiSync")
 )
 from Config import Config
+import pytest
 
 class configRepo:
-    def __init__(self):
-        self.apikey = 'testApiKey'
-        self.languageCode = 'testLanguageCode'
+    def __init__(self, apiKey, languageCode):
+        self.apikey = apiKey
+        self.languageCode = languageCode
         self.itemSet = None
     
     def getConfig(self, name):
@@ -15,30 +16,29 @@ class configRepo:
     
     def writeConfig(self, name, setTo):
         self.itemSet = setTo
+        
+@pytest.fixture
+def addonManager():
+    return configRepo("testApiKey", "testLanguageCode")
 
 class TestGets:
-    def test_should_get_api_key(self):
-        addonManager = configRepo()
+    def test_should_get_api_key(self, addonManager):
         result = Config(addonManager).GetApiKey()
         assert result == 'testApiKey'
         
-    def test_should_get_language_code(self):
-        addonManager = configRepo()
+    def test_should_get_language_code(self, addonManager):
         result = Config(addonManager).GetLanguageCode()
         assert result == 'testLanguageCode'
     
-    def test_should_get_deafult_status_to_interval(self):
-        addonManager = configRepo()
+    def test_should_get_deafult_status_to_interval(self, addonManager):
         result = Config(addonManager).GetStatusToInterval()
         assert result == {0: 1, 1: 5, 2: 10, 3: 20, 4: 40}
         
 class TestSets:
-    def test_should_set_api_key(self):
-        addonManager = configRepo()
+    def test_should_set_api_key(self, addonManager):
         Config(addonManager).SetApiKey('testSetApiKey')
         assert addonManager.itemSet['apiKey'] == 'testSetApiKey'
     
-    def test_should_set_language_code(self):
-        addonManager = configRepo()
+    def test_should_set_language_code(self, addonManager):
         Config(addonManager).SetLanguageCode('testSetLanguageCode')
         assert addonManager.itemSet['languageCode'] == 'testSetLanguageCode'
--- Tests/test_Converter.py
+++ Tests/test_Converter.py
@@ -2,70 +2,68 @@ import sys, os
 sys.path.append(
     os.path.realpath(f"{os.path.dirname(__file__)}/../LingqAnkiSync")
 )
-from Converter import ConvertAnkiCardsToLingqs, _ConvertAnkiIntervalToLingqStatus, _ConvertLingqStatusToAnkiInterval
+from Converter import ConvertAnkiCardsToLingqs, ConvertLingqsToAnkiCards, _ConvertAnkiIntervalToLingqStatus, _ConvertLingqStatusToAnkiInterval
 from Models import Lingq, AnkiCard
 import pytest
 
+@pytest.fixture
+def statusToInterval():
+    return {0:100, 1:200, 2:300, 3:400, 4:500}
+
 class TestIntervalToStatus:
-    def test_ConvertAnkiIntervalToLingqStatus(self):
-        testStatusToInterval = {0:100, 1:200, 2:300, 3:400, 4:500}
+    def test_ConvertAnkiIntervalToLingqStatus(self, statusToInterval):
         testInterval = 225
-        resultStatus = _ConvertAnkiIntervalToLingqStatus(testInterval, testStatusToInterval)
+        resultStatus = _ConvertAnkiIntervalToLingqStatus(testInterval, statusToInterval)
         assert resultStatus == 2
 
-    def test_should_return_max_status_if_interval_is_greater_than_max_interval(self):
-        testStatusToInterval = {0:100, 1:200, 2:300, 3:400, 4:500}
+    def test_should_return_max_status_if_interval_is_greater_than_max_interval(self, statusToInterval):
         testInterval = 600
-        resultStatus = _ConvertAnkiIntervalToLingqStatus(testInterval, testStatusToInterval)
+        resultStatus = _ConvertAnkiIntervalToLingqStatus(testInterval, statusToInterval)
         assert resultStatus == 4
     
-    def test_should_return_min_status_if_interval_is_less_than_min_interval(self):
-        testStatusToInterval = {0:100, 1:200, 2:300, 3:400, 4:500}
+    def test_should_return_min_status_if_interval_is_less_than_min_interval(self, statusToInterval):
         testInterval = 50
-        resultStatus = _ConvertAnkiIntervalToLingqStatus(testInterval, testStatusToInterval)
+        resultStatus = _ConvertAnkiIntervalToLingqStatus(testInterval, statusToInterval)
         assert resultStatus == 0
 
 class TestStatusToInterval:
-    def test_ConvertLingqStatusToAnkiInterval(self):
-        testStatusToInterval = {0:100, 1:200, 2:300, 3:400, 4:500}
+    def test_ConvertLingqStatusToAnkiInterval(self, statusToInterval):
         testStatus = 2
-        resultStatus = _ConvertLingqStatusToAnkiInterval(testStatus, testStatusToInterval)
+        resultStatus = _ConvertLingqStatusToAnkiInterval(testStatus, statusToInterval)
         assert resultStatus == 300
 
-    def test_should_return_max_interval_if_status_is_greater_than_max_status(self):
-        testStatusToInterval = {0:100, 1:200, 2:300, 3:400, 4:500}
+    def test_should_return_max_interval_if_status_is_greater_than_max_status(self, statusToInterval):
         testStatus = 5
-        resultStatus = _ConvertLingqStatusToAnkiInterval(testStatus, testStatusToInterval)
+        resultStatus = _ConvertLingqStatusToAnkiInterval(testStatus, statusToInterval)
         assert resultStatus == 500
     
-    def test_should_return_min_interval_if_status_is_less_than_min_stats(self):
-        testStatusToInterval = {0:100, 1:200, 2:300, 3:400, 4:500}
+    def test_should_return_min_interval_if_status_is_less_than_min_stats(self, statusToInterval):
         testStatus = -1
-        resultStatus = _ConvertLingqStatusToAnkiInterval(testStatus, testStatusToInterval)
+        resultStatus = _ConvertLingqStatusToAnkiInterval(testStatus, statusToInterval)
         assert resultStatus == 100
         
-class TestConvertAnkiToLingq:
-    def test_should_convert_ankiCard_to_Lingq(self):
-        testStatusToInterval = {0:100, 1:200, 2:300, 3:400, 4:500}
-        ankiCard = AnkiCard.AnkiCard(1, "word", "translation", 100)
-        modelLingq = Lingq.Lingq(1, "word", "translation", 0, None)
+        
+@pytest.fixture
+def Modelankicard():
+    return AnkiCard.AnkiCard(1, "word", "translation", 100)
 
-        resultLingq = ConvertAnkiCardsToLingqs([ankiCard], testStatusToInterval)[0]
-        assert resultLingq.primaryKey == modelLingq.primaryKey
-        assert resultLingq.word == modelLingq.word
-        assert resultLingq.translation == modelLingq.translation
-        assert resultLingq.status == modelLingq.status
+@pytest.fixture
+def Modellingq():
+    return Lingq.Lingq(1, "word", "translation", 0, None)
+
+class TestConvertAnkiToLingq:
+    def test_should_convert_ankiCard_to_Lingq(self, statusToInterval, Modelankicard, Modellingq):
+        resultLingq = ConvertAnkiCardsToLingqs([Modelankicard], statusToInterval)[0]
+        assert resultLingq.primaryKey == Modellingq.primaryKey
+        assert resultLingq.word == Modellingq.word
+        assert resultLingq.translation == Modellingq.translation
+        assert resultLingq.status == Modellingq.status
         assert resultLingq.extended_status is None
         
 class TestConvertLingqToAnki:
-    def test_should_convert_Lingq_to_AnkiCard(self):
-        testStatusToInterval = {0:100, 1:200, 2:300, 3:400, 4:500}
-        ankiCard = AnkiCard.AnkiCard(1, "word", "translation", 100)
-        modelLingq = Lingq.Lingq(1, "word", "translation", 0, None)
-
-        resultLingq = ConvertAnkiCardsToLingqs([ankiCard], testStatusToInterval)[0]
-        assert resultLingq.primaryKey == modelLingq.primaryKey
-        assert resultLingq.word == modelLingq.word
-        assert resultLingq.translation == modelLingq.translation
-        assert resultLingq.status == modelLingq.status
-        assert resultLingq.extended_status is None
+    def test_should_convert_Lingq_to_AnkiCard(self, statusToInterval, Modelankicard, Modellingq):
+        resultAnkiCard = ConvertLingqsToAnkiCards([Modellingq], statusToInterval)[0]
+        assert resultAnkiCard.primaryKey == Modelankicard.primaryKey
+        assert resultAnkiCard.word == Modelankicard.word
+        assert resultAnkiCard.translation == Modelankicard.translation
+        assert resultAnkiCard.interval == Modelankicard.interval
