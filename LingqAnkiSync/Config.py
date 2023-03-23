58a7263c15abc847455956747c42b05202c9a274
Tyler Hagstrom <tyler.hagstrom@gmail.com>
2023-03-23 09:13:53 -0500

    Update default intervals

--- LingqAnkiSync/Config.py
+++ LingqAnkiSync/Config.py
@@ -24,4 +24,4 @@ class Config:
         self._SetConfig('languageCode', setTo)
     
     def GetStatusToInterval(self):
-        return {0: 1, 1: 5, 2: 10, 3: 20, 4: 40}
+        return {0: 0, 1: 10, 2: 15, 3: 25, 4: 50}
