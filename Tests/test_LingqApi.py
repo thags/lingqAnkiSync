bf2b0297b37e69cd6a41c3a41452e1b04e394a04
Oleksiy Kovyrin <oleksiy@kovyrin.net>
2025-03-16 11:07:58 -0400

    Clean up tests

--- Tests/test_LingqApi.py
+++ Tests/test_LingqApi.py
@@ -5,26 +5,16 @@ sys.path.append(
     os.path.realpath(f"{os.path.dirname(__file__)}/../LingqAnkiSync")
 )
 from LingqApi import LingqApi
-import pytest
 
 class TestLingqApi:
     def test_should_set_import_knowns_to_true_by_default(self):
-        # Arrange & Act
         api = LingqApi("test_key", "test_lang")
-
-        # Assert
         assert api.import_knowns == True
 
     def test_should_set_import_knowns_to_false_when_specified(self):
-        # Arrange & Act
         api = LingqApi("test_key", "test_lang", import_knowns=False)
-
-        # Assert
         assert api.import_knowns == False
 
     def test_should_set_import_knowns_to_true_when_specified(self):
-        # Arrange & Act
         api = LingqApi("test_key", "test_lang", import_knowns=True)
-
-        # Assert
         assert api.import_knowns == True
